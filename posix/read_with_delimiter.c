d1b13e63a81427874e476f7ebe33fe11a0a587bd
Washoutx <patryk.kups.priv@gmail.com>
2025-11-24 18:05:41 +0100

    delimiter

---
+++ posix/read_with_delimiter.c
@@ -0,0 +1,47 @@
+#include <limits.h>
+#include <stdio.h>
+
+#define VALIDATE_RETURN(ret, msg)                                              \
+  do {                                                                         \
+    if (ret < 0) {                                                             \
+      perror(msg);                                                             \
+      return -1;                                                               \
+    }                                                                          \
+  } while (0)
+
+int main() {
+  int ret = 0;
+  FILE *file = fopen("data.txt", "w+");
+  if (NULL == file) {
+    perror("fopen failed");
+    return -1;
+  }
+
+  ret = fprintf(file, "test,data,file");
+  VALIDATE_RETURN(ret, "fprintf failed");
+  ret = fflush(file);
+  VALIDATE_RETURN(ret, "fflush failed");
+  ret = fseek(file, 0, SEEK_SET);
+  VALIDATE_RETURN(ret, "fseek failed");
+
+  size_t size = LINE_MAX;
+  char buffer[size];
+  char *s = buffer;
+  char delimiter = ',';
+
+  while (--size > 0 && (ret = fgetc(file)) != EOF && (*s++ = ret) != delimiter)
+    ;
+  ;
+
+  if (ret == delimiter) {
+    *(--s) = '\0';
+  } else {
+    *s = '\0';
+  }
+
+  printf("data=%s", buffer);
+
+  ret = fclose(file);
+  VALIDATE_RETURN(ret, "fclose failed");
+  return 0;
+}
